c9aa8d6c267ba7551eb14d92be7c5c3801401170
devil1321 <s.dominik1321@gmail.com>
2021-06-14 14:41:08 +0200

    json server init

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const jsonServer = require('json-server');
+const express = require('express')
+const cors = require('cors')
 const server = jsonServer.create();
 const router = jsonServer.router('movies.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
 
+const app = express()
+app.use(cors())
 server.use(middlewares);
 server.use(router);
 
